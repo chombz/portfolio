6d954ff659de82d4b0a6fe8711ce0ba3ef53e7e9
chombz <chombareuben@gmail.com>
2026-04-17 00:31:55 +0300

    Delete Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 // src/App.jsx - FINAL VERSION
-import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
@@ -8,10 +7,10 @@ import ContactForm from './components/ContactForm/ContactForm';
 import Contact from './components/Contact/Contact';
 import './App.css';
 
-function App() {
+function App()
+{
   return (
     <div className="app">
-      <Header />
       <main>
         <Hero />
         <About />
